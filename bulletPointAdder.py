93660686cf60c4965abecb506cda9b5073db451c
Nika <122809340+nikabilanych@users.noreply.github.com>
2023-09-18 17:00:39 +0200

    bulletPointAdder.py

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -1,21 +1,16 @@
 #! python3
-#add Wikipedia bullet points to each line of text in a file
+
+
+#add bullet points to text from the clipboard
 import pyperclip
-
 text=pyperclip.paste()
-#text has been pasted
 
-#split by \n, return list
 lines=text.split('\n')
-
-
-#now let's do something to it
-#list of modified string
 for i in range(len(lines)):
     lines[i]='* '+lines[i]
 text='\n'.join(lines)
 pyperclip.copy(text)
-#join the list back together, the glue being yet again, \n
+#return copied content back into the clipboard w the bullet points
 
 
 
