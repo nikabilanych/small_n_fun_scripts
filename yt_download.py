95d893fb1ab384dccc7c4db088d6de485c9b6f87
Nika <nikabilanych@gmail.com>
2023-10-31 14:22:24 +0100

    Add files via upload

--- yt_download.py
+++ yt_download.py
@@ -1,10 +1,11 @@
 #! python3
 from pytube import YouTube
 from sys import argv, exit
-
-if argv[1]:
-    yt = YouTube(argv[1])
-
+link=input("Enter the link you'd like to download:")
+video=YouTube(link)
+print(f"Proceed to download :{video.title}?")
+activator=
+if video:
     video = yt.streams.get_highest_resolution()
 
     video.download()
