e5a68f4c3f823c7db53b5295bf7224960d1c73da
Nika Bilanych <nikabilanych@Nikas-MacBook-Air.local>
2023-11-02 17:45:34 +0100

    error handling

--- yt_download.py
+++ yt_download.py
@@ -1,14 +1,16 @@
 #! python3
 from pytube import YouTube
-from sys import argv, exit
+
 link=input("Enter the link you'd like to download:")
 video=YouTube(link)
 print(f"Proceed to download :{video.title}?")
-activator=
-if video:
-    video = yt.streams.get_highest_resolution()
+
+if (input("[y/n] "))=="y":
+    resolution=input()
+    print(f"Enter the resolution {i for i}
+    video.streams.get_highest_resolution()
 
     video.download()
     print("Download Completed")
-    exit()
+
 print("invalid")
