dc6c7fe03480cc2476a190eaeda44fc3ee5c865b
nikabilanych <nikabilanych@gmail.com>
2023-11-02 18:04:34 +0100

    Error handling
    1/2 of it

--- yt_download.py
+++ yt_download.py
@@ -1,16 +1,20 @@
 #! python3
 from pytube import YouTube
+from sys import exit
 
-link=input("Enter the link you'd like to download:")
-video=YouTube(link)
+link = input("Enter the link you'd like to download:")
+try:
+    video = YouTube(link)
+except Exception as e:
+    print("Invalid link %s. Try again", e)
+    exit()
 print(f"Proceed to download :{video.title}?")
 
-if (input("[y/n] "))=="y":
-    resolution=input()
-    print(f"Enter the resolution {i for i}
-    video.streams.get_highest_resolution()
+if (input("[y/n] ")) == "y":
+    print(f"Enter the resolution: {i for i in video.streams.all()}")
+    resolution = input()
+    video.streams.get_by_resolution(resolution)
 
-    video.download()
     print("Download Completed")
 
 print("invalid")
