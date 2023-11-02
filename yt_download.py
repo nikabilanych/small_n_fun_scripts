c01486d26e9a0e5894e470553d39b01c094ec472
nikabilanych <nikabilanych@gmail.com>
2023-11-02 18:23:00 +0100

    touchup

--- yt_download.py
+++ yt_download.py
@@ -6,13 +6,12 @@ link = input("Enter the link you'd like to download:")
 try:
     video = YouTube(link)
 except Exception as e:
-    print("Invalid link %s. Try again", e)
+    print("Invalid link. Try again:", e)
     exit()
 print(f"Proceed to download :{video.title}?")
 
 if (input("[y/n] ")) == "y":
-
-    for i,j in enumerate(video.streams, 1):
+    for i, j in enumerate(video.streams, 1):
         print(i, j.resolution)
     resolution = input("Enter the resolution:")
     video.streams.get_by_resolution(resolution)
